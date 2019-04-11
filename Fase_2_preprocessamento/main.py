f95da68438a56888b12e85301baa5d7939b8ceba
TiagoLeite <tiago.tmleite@gmail.com>
2019-04-11 11:01:27 -0300

    generating submissions for all models

--- Fase_2_preprocessamento/main.py
+++ Fase_2_preprocessamento/main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import math
-import numpy as np
 import seaborn as sns
 from sklearn import preprocessing
 from scipy import stats
@@ -14,12 +13,12 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn import preprocessing
 from sklearn.datasets import load_digits
 from sklearn.feature_selection import SelectKBest, chi2
-
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.base import clone
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
-
 
 remov = {}
 remov["corr_filter"] = []
@@ -66,7 +65,7 @@ def corr_filter(data, target):
 
 
 # Filter: unbalanced classes
-def desc_filter(data):
+def unbalanced_filter(data):
     global remov
     objects = []
     classes_attr = {}
@@ -117,8 +116,8 @@ def wrapper_subconj_kbest(data, k=20):
     X = data
     X = X.drop('SalePrice', axis=1)
     y = data['SalePrice']
-    x_new = SelectKBest(chi2, k).fit_transform(X, y)
-    return x_new, y
+    selected_features = SelectKBest(chi2, k).fit(X, y).get_support()
+    return selected_features, y
 
 
 def remove_redundant(data):
@@ -140,57 +139,178 @@ def remove_redundant(data):
     return data_new
 
 
-def transform_to_numeric(data):
+def transform_to_numeric(data, should_remove_redundant=False):
+    if should_remove_redundant:
+        data = remove_redundant(data)
 
-    data_new = remove_redundant(data)
-    col = list(data_new.columns.values)
+    col = list(data.columns.values)
 
     # Tranformação dos atributos
     for c in col:
         le = preprocessing.LabelEncoder()
-        if not is_numeric_dtype(data_new[c]):
-            vals = data_new[c].unique()
+        if not is_numeric_dtype(data[c]):
+            vals = data[c].unique()
             le.fit(vals)
-            data_new[c] = le.transform(data_new[c])
-
-    return data_new
-
-
-def apply_filters(data):
-
-    data = nan_filter(data)
-    print(len(data.columns))
-
-    data = corr_filter(data, "SalePrice")
-    print(len(data.columns))
-
-    data = desc_filter(data)
-    print(len(data.columns))
+            data[c] = le.transform(data[c])
 
     return data
 
 
-def main():
+def apply_filters(data, nan=False, corr=False, unbalanced=False, is_training_data=True):
+    if nan:
+        print("Nan filter...")
+        data = nan_filter(data)
+        print(len(data.columns))
 
+    if corr:
+        if is_training_data:
+            data = corr_filter(data, "SalePrice")
+            print(len(data.columns))
+
+    if unbalanced:
+        data = unbalanced_filter(data)
+        print(len(data.columns))
+
+    return data
+
+
+def run_model_on_filters(model, data, test, nan_filter=False, corr_filter=False,
+                         unbalanced_filter=False):
+    data = apply_filters(data, nan=nan_filter, corr=corr_filter, unbalanced=unbalanced_filter)
+    test = apply_filters(test, nan=nan_filter, corr=corr_filter, unbalanced=unbalanced_filter,
+                         is_training_data=False)
+
+    for column in test.columns:
+        if column not in data.columns:
+            test = test.drop(column, axis=1)
+
+    print(test.shape, data.shape)
+
+    data = transform_to_numeric(data, True)  # put True for corr filter, False otherwise
+    test = transform_to_numeric(test, True)
+
+    data_y = data['SalePrice']
+    data_x = data.drop(['SalePrice'], axis=1)
+
+    model.fit(data_x, data_y)
+
+    predictions = model.predict(test)
+
+    return predictions
+
+
+def run_model_on_rfe_wrapper(model, data, test):
+    data = remove_redundant(data)
+    data = transform_to_numeric(data)
+
+    test = remove_redundant(test)
+    test = transform_to_numeric(test)
+
+    data_x, data_y = wrapper_rfe(data)
+
+    print('RFE Wrapper:')
+
+    for column in test.columns:
+        if column not in data_x.columns:
+            test = test.drop(column, axis=1)
+
+    data_x = data_x.drop(['SalePrice'], axis=1)
+
+    print(data_x.shape, test.shape)
+
+    model.fit(data_x, data_y)
+
+    predictions = model.predict(test)
+
+    return predictions
+
+
+def run_model_on_wrapper_subconj_kbest(model, data, test):
+    data = remove_redundant(data)
+    data = transform_to_numeric(data)
+
+    test = remove_redundant(test)
+    test = transform_to_numeric(test)
+
+    print('KBest Wrapper:')
+
+    features, data_y = wrapper_subconj_kbest(data)
+    features = list(features)
+    print(features)
+
+    to_drop = []
+
+    for k in range(len(features)):
+        if not features[k]:
+            to_drop.append(k)
+
+    data = data.drop(data.columns[to_drop], axis=1)
+
+    data = data.drop(['SalePrice'], axis=1)
+
+    print(data.shape)
+
+    model.fit(data, data_y)
+
+    for column in test.columns:
+        if column not in data.columns:
+            test = test.drop(column, axis=1)
+
+    predictions = model.predict(test)
+
+    return predictions
+
+
+def main():
     data = pd.read_csv("train.csv")
+    data_copy = data
+
+    test = pd.read_csv('test.csv')
+    test_ids = test['Id']
+
     data = data.drop(columns='Id')
     print('Train shape:', data.shape)
 
-    '''data = apply_filters(data)
-    print(data.shape)
-    print(data.columns)'''
+    # prediction model:
+    model = GradientBoostingRegressor(
+        n_estimators=4864,
+        learning_rate=0.05,
+        max_depth=4,
+        max_features='log2',
+        min_samples_leaf=15,
+        min_samples_split=10,
+        loss='huber',
+        random_state=5)
 
-    data_new = remove_redundant(data)
-    data_new = transform_to_numeric(data_new)
-    # print(data_new.head())
+    # NaN Filter
+    predictions = run_model_on_filters(clone(model), data, test, nan_filter=True)
+    submission = pd.DataFrame({'Id': test_ids, 'Saleprice': predictions})
+    submission.to_csv('submissions/nan_filter_submission.csv', index=False)
+    print('Nan Filter - Submission file successfully created!')
 
-    # wrapper rfe
-    data_x, data_y = wrapper_rfe(data_new)
-    print(data_x.shape, data_y.shape)
+    # Correlation filter
+    predictions = run_model_on_filters(clone(model), data, test, corr_filter=True)
+    submission = pd.DataFrame({'Id': test_ids, 'Saleprice': predictions})
+    submission.to_csv('submissions/corr_filter_submission.csv', index=False)
+    print('Correlation Filter - Submission file successfully created!')
 
-    # wrapper subconjunto
-    data_x, data_y = wrapper_subconj_kbest(data_new)
-    print(data_x.shape, data_y.shape)
+    # Unbalanced filter
+    predictions = run_model_on_filters(clone(model), data, test, unbalanced_filter=True)
+    submission = pd.DataFrame({'Id': test_ids, 'Saleprice': predictions})
+    submission.to_csv('submissions/unbalanced_filter_submission.csv', index=False)
+    print('Unbalanced Filter - Submission file successfully created!')
+
+    # Wrapper rfe
+    predictions = run_model_on_rfe_wrapper(clone(model), data, test)
+    submission = pd.DataFrame({'Id': test_ids, 'Saleprice': predictions})
+    submission.to_csv('submissions/rfe_wrapper_submission.csv', index=False)
+    print('RFE Wrapper: - Submission file successfully created!')
+
+    # Wrapper KBest
+    predictions = run_model_on_wrapper_subconj_kbest(clone(model), data, test)
+    submission = pd.DataFrame({'Id': test_ids, 'Saleprice': predictions})
+    submission.to_csv('submissions/KBest_wrapper_submission.csv', index=False)
+    print('KBest Wrapper: - Submission file successfully created!')
 
 
 if __name__ == '__main__':
